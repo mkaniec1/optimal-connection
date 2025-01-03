4ac0958dca7b1514ccad2235e4158752f47401d0
Konrad <realagmag@op.pl>
2025-01-03 15:21:00 +0100

    Added data and script loading it to db

---
+++ backend/pop/optimal_path/load_data.py
@@ -0,0 +1,72 @@
+from optimal_path.models import Node, Connection, Channel
+import csv
+
+
+def load_nodes():
+    with open("optimal_path/initial_data/wezly.csv", "r") as file:
+        reader = csv.reader(file, delimiter=";")
+        headers = next(reader)
+        for row in reader:
+            node_id, latitude, longitude = row
+            Node.objects.create(
+                id=int(node_id),
+                latitude=float(latitude.replace(",", ".")),
+                longitude=float(longitude.replace(",", "."))
+            )
+
+
+def load_connections():
+    with open("optimal_path/initial_data/zajetosc.csv", "r") as file:
+        reader = csv.reader(file, delimiter=";")
+        headers = next(reader)
+        for row in reader:
+            connection_id = row[0]
+            starting_node = Node.objects.get(id=row[1])
+            ending_node = Node.objects.get(id=row[2])
+            total_capacity = row[3][:-4]  # to remove " THz"
+            provisioned_capacity = row[4]
+
+            Connection.objects.create(
+            id=connection_id,
+            starting_node=starting_node,
+            ending_node=ending_node,
+            total_capacity=total_capacity,
+            provisioned_capacity=provisioned_capacity,
+            )
+
+
+def load_channels():
+    with open("optimal_path/initial_data/spectrum_kanaly.csv", "r") as file:
+        reader = csv.reader(file, delimiter=";")
+        headers = next(reader)
+        artifact = "-snc::controlPlane"
+        for row in reader:
+            connection_id = row[0]
+            if not row[1] or not row[2] or not row[3]:
+                print("no data")
+                continue
+            request_free_id = row[1]
+            frequency = row[2]
+            width = float(row[3])
+            if width == 37.5:
+                width = 50.0
+            elif width == 59.0:
+                width = 75.0
+            elif width == 101.8:
+                width = 112.5
+            else:
+                raise ValueError(f"unexpected value: {width}")
+            # row[4] skipped
+            channel = row[5]
+            Channel.objects.create(
+                connection=Connection.objects.filter(id=connection_id)[0],
+                request_free_id=request_free_id,
+                frequency=frequency,
+                width=width,
+                channel=channel,
+            )
+
+def load_all():
+    load_nodes()
+    load_connections()
+    load_channels()
---
+++ backend/pop/optimal_path/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Node(models.Model):
+    latitude = models.DecimalField("Latitude", max_digits=16, decimal_places=14)
+    longitude = models.DecimalField("Longitude", max_digits=16, decimal_places=14)
+
+
+class Connection(models.Model):
+    starting_node = models.ForeignKey(Node, on_delete=models.DO_NOTHING, related_name="start_connections")
+    ending_node = models.ForeignKey(Node, on_delete=models.DO_NOTHING, related_name="end_connections")
+    total_capacity = models.DecimalField("Total capacity", max_digits=4, decimal_places=1)
+    provisioned_capacity = models.DecimalField("Provisioned capacity", max_digits=5, decimal_places=2)
+
+
+class Channel(models.Model):
+    connection = models.ForeignKey(Connection, on_delete=models.DO_NOTHING)
+    request_free_id = models.CharField("Request Free Id", max_length=64)
+    frequency = models.DecimalField("Frequency", max_digits=9, decimal_places=6)
+    width = models.DecimalField("Width", max_digits=4, decimal_places=1)
+    channel = models.CharField("Channel name", max_length=5, null=True)
